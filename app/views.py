772c789c356e9282ce4e3850e12f0020f49867d7
pjrein <peter@peter-desktop.localdomain>
2021-01-22 11:40:56 +0100

    git init

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from app import app
+
+from flask import render_template, request
+
+
+@app.route("/")
+def index():
+
+    args = None
+
+    if request.args:
+
+        args = request.args
+
+        return render_template("public/index.html", args=args)
+
+    return render_template("public/index.html", args=args)
+
+
